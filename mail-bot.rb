57ff800765b2c91aec49ad0e10f56d97f965cc43
Gianluca Venturini <gianluca.91@gmail.com>
2015-04-06 15:59:20 -0500

    Added new Nutella APIs

--- mail-bot.rb
+++ mail-bot.rb
@@ -2,8 +2,13 @@ require 'nutella_lib'
 require 'json'
 
 
+
+# Parse command line arguments
+broker, app_id, run_id = nutella.parse_args ARGV
+# Extract the component_id
+component_id = nutella.extract_component_id
 # Initialize nutella
-nutella.init("crepe", "localhost", "mail-bot")
+nutella.init(broker, app_id, run_id, component_id)
 
 puts "Mail bot initialization"
 
