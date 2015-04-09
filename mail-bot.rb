ef102c803d7e0ea2150fdee79d316f14fe1163c7
Gianluca Venturini <gianluca.91@gmail.com>
2015-04-09 18:04:36 -0500

    Bug fix

--- mail-bot.rb
+++ mail-bot.rb
@@ -14,35 +14,66 @@ nutella.init(broker, app_id, run_id, component_id)
 
 puts "Mail bot initialization"
 
-nutella.net.handle_requests('mail/send', lambda do |request, from|
-	puts "Send mail to " + request['to'] + "message: " + request['message']
+nutella.net.handle_requests('mail/send_subscription', lambda do |request, from|
+	puts "Send mail to " + request['to'] + "message: " + request['componentName']
 
   template_name = 'Nutella subscription'
   template_content = [
       {
-          "name"=> "main",
-          "content"=> "Congratulation: subscription to component
+          :name=> 'main',
+          :content=> "Congratulation: subscription to component
                     <b>#{request['componentName']}</b>
                     has been succeded."
       }
   ]
   message = {
-      :subject=> "Notification subscription success",
-      :from_name=> "Nutella Monitoring Interface",
+      :subject=> 'Notification subscription success',
+      :from_name=> 'Nutella Monitoring Interface',
       :to=>[
           {
               :email=> request['to'],
               # :name=> ""
           }
       ],
-      :from_email=>"nutellamonitoring@gmail.com"
+      :from_email=>'nutellamonitoring@gmail.com'
   }
 
   sending = m.messages.send_template template_name, template_content, message
   puts sending
 
 
-  {"status" => "ok"}
+  {:status => 'ok'}
+end)
+
+nutella.net.handle_requests('mail/send_notification', lambda do |request, from|
+  puts "Send mail to " + request['to'] + "message: " + request['componentName']
+
+  template_name = 'Nutella subscription'
+  template_content = [
+      {
+          :name=> 'main',
+          :content=> "Congratulation: subscription to component
+                      <b>#{request['componentName']}</b>
+                      has been succeded."
+      }
+  ]
+  message = {
+      :subject=> 'Notification subscription success',
+      :from_name=> 'Nutella Monitoring Interface',
+      :to=>[
+          {
+              :email=> request['to'],
+              # :name=> ""
+          }
+      ],
+      :from_email=>'nutellamonitoring@gmail.com'
+  }
+
+  sending = m.messages.send_template template_name, template_content, message
+  puts sending
+
+
+  {:status => 'ok'}
 end)
 
 puts "Initialization completed"
