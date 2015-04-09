f33d458a2d39647661effcd88fec6b4189c90022
andreuke <andreatello92@gmail.com>
2015-04-09 17:48:11 -0500

    mail send

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'mandrill-api', '~> 1.0.53'
--- mail-bot.rb
+++ mail-bot.rb
@@ -1,5 +1,7 @@
 require 'nutella_lib'
 require 'json'
+require 'mandrill'
+m = Mandrill::API.new 'i4frQYcTBMyZ6bYyLkEVOQ'
 
 
 # Initialize nutella
@@ -10,7 +12,32 @@ puts "Mail bot initialization"
 nutella.net.handle_requests('mail/send', lambda do |request, component_id, resource_id|
 	puts "Send mail to " + request['to'] + "message: " + request['message']
 
-	{"status" => "ok"}
+  template_name = 'Nutella subscription'
+  template_content = [
+      {
+          "name"=> "main",
+          "content"=> "Congratulation: subscription to component
+                    <b>#{request['componentName']}</b>
+                    has been succeded."
+      }
+  ]
+  message = {
+      :subject=> "Notification subscription success",
+      :from_name=> "Nutella Monitoring Interface",
+      :to=>[
+          {
+              :email=> request['to'],
+              # :name=> ""
+          }
+      ],
+      :from_email=>"nutellamonitoring@gmail.com"
+  }
+
+  sending = m.messages.send_template template_name, template_content, message
+  puts sending
+
+
+  {"status" => "ok"}
 end)
 
 puts "Initialization completed"
